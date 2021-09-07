d6de1fb48283a4ab90617d8c56477ca111b4baf6
Tiago Viezzoli <tiagoviezzoli@gmail.com>
2021-09-07 01:32:00 -0300

    New features

--- index.py
+++ index.py
@@ -126,16 +126,146 @@ def calculadora():
 
 
 # 3.2
-
+def definirTipoNumero(numero):
+    if(numero % 1 == 0 and numero < 0):
+        mensaje = "Entero"
+    elif(numero % 1 == 0):
+        mensaje = "Entero natural"
+    else:
+        mensaje = "Real"
+    return mensaje
+ # 3.2
 def definirNumero(numero):
     if(numero == 0 ):
-        mensaje = "El numero es 0 y entero"
+        mensaje = "0 y entero"
+    elif(numero > 0):
+        concat = definirTipoNumero(numero)
+        mensaje = "Positivo ", concat
+    else:
+        concat = definirTipoNumero(numero)
+        mensaje = "Negativo " , concat        
     
     return mensaje
 
+# 3.2
 def ingresarNumeroEvaluacion():
-     numero = int(input("Ingrese un numero: "))
-     definirNumero(numero)
+     numero = float(input("Ingrese un numero: "))
+     resultado = definirNumero(numero)
+     print(resultado)
+
+
+ # 3.3
+def validarFecha(dia,mes,anio):
+    bisiesto = (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0)
+    validMesDia = dia > 0 and dia <= 31 and mes <= 12
+    print("Es bisiesto", bisiesto)
+    if(mes == 2 and bisiesto and dia <= 29 and validMesDia):
+        valido = True
+    elif(mes == 2 and bisiesto != True and dia <= 28 and validMesDia):
+        valido = True
+    elif(validMesDia):
+        valido = True
+    else:
+        valido = False
+    
+    return valido
+    
+
+# 3.3
+def ingresarFecha():
+      dia = int(input("Ingrese el dia: "))
+      mes = int(input("Ingrese el mes: "))
+      anio = int(input("Ingrese el anio: "))
+      fechaValida = validarFecha(dia,mes,anio)
+      print(fechaValida) 
+
+
+#3.4
+def validarDistanciaNumeros(numeroUno,numeroDos,numeroTres):
+    if(numeroUno > numeroDos and numeroUno > numeroTres):
+        if(numeroDos > numeroTres):
+            numeroMayor = numeroUno
+            numeroMenor = numeroTres
+            numeroRestante = numeroDos
+        else:
+            numeroMayor = numeroUno
+            numeroMenor = numeroDos
+            numeroRestante = numeroTres  
+    elif(numeroDos > numeroUno and numeroDos > numeroTres):
+        if(numeroUno > numeroTres):
+            numeroMayor = numeroDos
+            numeroMenor = numeroTres
+            numeroRestante = numeroUno
+        else:
+            numeroMayor = numeroDos
+            numeroMenor = numeroUno
+            numeroRestante = numeroTres
+    else:
+        numeroMayor = numeroTres
+        if(numeroUno > numeroDos):
+            numeroMenor = numeroDos
+            numeroRestante = numeroUno
+        else:
+            numeroMenor = numeroUno
+            numeroRestante = numeroDos
+    if((numeroMenor + numeroMayor) / numeroRestante == 2):
+        distancia = "Si"
+    else:
+        distancia = "No"
+    return distancia
+
+
+# 3.4
+def ingresarNumerosExtremos():
+    numeroUno = int(input("Ingrese el numero uno: "))
+    numeroDos = int(input("Ingrese el numero dos: "))
+    numeroTres = int(input("Ingrese el numero tres: "))
+    resultado = validarDistanciaNumeros(numeroUno,numeroDos,numeroTres)
+    print(resultado)
+
+# 3.5
+def bono(sueldoBase):
+    if(sueldoBase > 2000):
+        valorBono = sueldoBase * 0.15
+    else:
+        valorBono = sueldoBase * 0.20
+    return valorBono
+#3.5
+def plusH(sueldoBase, tieneHijos):
+    if(tieneHijos == "si"):
+        plus = sueldoBase * 0.05
+    else:
+        plus = 0
+    return plus
+#3.5
+def plusC(sueldoBase,categoria):
+    if(categoria == 1 or categoria == 2 or categoria == 3):
+        plus = sueldoBase * 0.10
+    elif(categoria == 4 or categoria == 5 or categoria == 6):
+        plus = sueldoBase * 0.12
+    else:
+        plus = sueldoBase * 0.20
+    return plus
+
+#3.5
+def plus(sueldoBase,tieneHijos,categoria):
+    plusHijos = plusH(sueldoBase,tieneHijos)
+    plusCategoria = plusC(sueldoBase,categoria)
+    if(tieneHijos == "si" and (categoria != "7" and categoria != "8" and categoria != "9")):
+       plusFinal = plusHijos + plusCategoria
+    else:
+        plusFinal = plusCategoria
+    return plusFinal
+
+#3.5
+def ingresarInformacionSueldo():
+     sueldoBase = float(input("Ingrese su sueldo base: "))
+     tieneHijos = input("Tiene Hijos: ")
+     categoria = int(input("Ingrese a que categoria pertenece: "))
+     valorBono = bono(sueldoBase)
+     otroBono = plus(sueldoBase,tieneHijos,categoria)
+     print(sueldoBase,valorBono,otroBono)
+     print(sueldoBase + valorBono + otroBono)
 
 def main():
     #1 Entrada/Salida/Variables/Operadores
@@ -149,7 +279,11 @@ def main():
     #numeroAleatorio()  #2.4
     # 3 Condicionales
     #calculadora()  3.1
-    ingresarNumeroEvaluacion() # 3.2
+    #ingresarNumeroEvaluacion() #3.2
+    #ingresarFecha() #3.3
+    ingresarNumerosExtremos() # 3.4
+    #ingresarInformacionSueldo() # 3.5
+   
 
 
 main()    
